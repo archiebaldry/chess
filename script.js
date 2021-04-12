336eb847bdd7fc7eff622f44f81ba7902783bacd
Archie Baldry <33762310+archiebaldry@users.noreply.github.com>
2021-04-12 18:02:12 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const BOARD = document.getElementById("board");
+
+var board = [
+	"br", "bn", "bb", "bq", "bk", "bb", "bn", "br",
+	"bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp",
+	"", "", "", "", "", "", "", "",
+	"", "", "", "", "", "", "", "",
+	"", "", "", "", "", "", "", "",
+	"", "", "", "", "", "", "", "",
+	"wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp",
+	"wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"
+];
+var selectedCell = "";
+
+drawBoard();
+
+function drawBoard() {
+	index = 0;
+	for (row of BOARD.rows) {
+		for (cell of row.cells) {
+			piece = board[index];
+			if (piece == "") {
+				cell.innerHTML = "";
+			}
+			else {
+				cell.innerHTML = "<img src='pieces/" + piece + ".svg'>";
+			}
+			index += 1;
+		}
+	}
+}
+
+function clickedCell(cell) {
+	console.clear();
+	console.log("Selected pos: " + selectedCell);
+	console.log("Clicked pos: " + cell.id);
+	console.log("Clicked innerHTML: " + cell.innerHTML);
+	if (cell.id == selectedCell) {
+		console.log("Deselecting cell...");
+		selectedCell = "";
+	}
+	else {
+		console.log("Selecting cell...");
+		selectedCell = cell.id;
+	}
+}
